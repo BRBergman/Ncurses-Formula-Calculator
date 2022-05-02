e57ab4931cef09ffc0a81425e5b77b9bba171125
BRBergman <blake.bergman@gmail.com>
2022-05-02 11:24:41 -0400

    master

--- src/maths.h
+++ src/maths.h
@@ -8,26 +8,18 @@ double distance(float x1,float y1,float x2,float y2);
 
 double slope(float x1,float y1,float x2,float y2);
 
-
-
 double pythagorean(float a, float b);
-
 
 double reversepythagorean(float b, float c);
 
-
 double midseg(float s1,float s2);
-
 
 double quadperm(float x1,float y1,float x2,float y2, float x3,float y3,float x4,float y4);
 
-
 int angles(int sides);
-
 
 int cheiftr(float a, float b, float c);
 
 double circ(float rad);
-
 
 double carea(float rad);
