52297641a53c93759764d090e10f7d672f09219f
BRBergman <blake.bergman@gmail.com>
2022-06-16 09:55:08 -0400

    edited comment

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 #include "ncextra.h"
 #include "invert.h"
 
-int row, col;		/* to store the number of rows and the number of colums of the screen (i know its a global variable but like deal with it)*/
+int row, col;		/* to store the number of rows and the number of colums of the screen 
+(i know its a global variable but like deal with it)*/
 
 int csar()
 {
