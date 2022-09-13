a3800de2d4b1fb6e932ec2990ec0dbb8ffd78ee7
BRBergman <blake.bergman@gmail.com>
2022-09-13 11:01:41 -0400

    made more clean

--- src/ncextra.cpp
+++ src/ncextra.cpp
@@ -20,22 +20,22 @@ int ask()
 	
 	clear();
 	// add new part here to show up 
-	const char* askev[] = {"press 0 to quit",
-	"press 1 to do the pythagorean theorem ",
-	"press 2 to do the inverse pythagorean theorem",
-	"press 3 to do the distance formula",
-	"press 4 to do the slope formula", 
-	"press 5 to classify a triangle",
-	"press 6 to check the circumference of a circle",
-	"press 7 to check the area of a circle",
-	"press 8 to preform the midpoint formula",
-	"press 9 to find the area of a certan section of a circle",
+	const char* askev[] = {"quit",
+	"do the pythagorean theorem ",
+	"do the inverse pythagorean theorem",
+	"do the distance formula",
+	"do the slope formula", 
+	"classify a triangle",
+	"check the circumference of a circle",
+	"check the area of a circle",
+	"preform the midpoint formula",
+	"find the area of a certan section of a circle",
 	
 	NULL};
 	
 	for (int i = 0; askev[i] != NULL; i++ )
 	{
-		mvprintw(row/2-6+i, (col-strlen(askev[i]))/2, "%s",askev[i] );
+		mvprintw(row/2-6+i, (col-strlen(askev[i])-11)/2, "Press %i to %s",i,askev[i] );
 		mvprintw(row/2-2+i,col/2-1, " ");
 	}
 	char numin[10];
