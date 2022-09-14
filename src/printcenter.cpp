0fe3beba046b0f0b68d00de6d3cf7da7b34a09be
BRBergman <blake.bergman@gmail.com>
2022-09-14 12:48:01 -0400

    master

---
+++ src/printcenter.cpp
@@ -0,0 +1,33 @@
+#include "printcenter.h"
+
+void printcenter(std::string in)
+{
+	mvprintw(row/2,(col-strlen(in.c_str()))/2,"%s",in.c_str()); //print string to center of screen
+	return;
+}
+
+void printcenter(char in[])
+{
+	mvprintw(row/2,(col-strlen(in))/2,"%s",in); //print char[] to center of screen
+	return;
+}
+
+void printcenter(float in)
+{
+	int conv = std::to_string(in).length();
+	mvprintw(row/2,(col-conv)/2,"%d",in); //print float to center of screen
+	return;
+}
+void printcenter(double in)
+{
+	int conv = std::to_string(in).length();
+	mvprintw(row/2,(col-conv)/2,"%d",in); //print double to center of screen
+	return;
+}
+void printcenter(int in)
+{
+	int conv = std::to_string(in).length();
+	mvprintw(row/2,(col-conv)/2,"%i",in); //print int to center of screen
+	return;
+}
+
---
+++ src/printcenter.h
@@ -0,0 +1,23 @@
+#ifndef PRINTCENTER_H
+#define PRINTCENTER_H
+#include <ncurses.h>
+#include <iostream>
+#include "ncextra.h"
+
+void printcenter(std::string in);
+
+void printcenter(char in[]);
+
+void printcenter(float in);
+
+void printcenter(double in);
+
+void printcenter(int in);
+
+
+
+
+
+
+
+#endif
