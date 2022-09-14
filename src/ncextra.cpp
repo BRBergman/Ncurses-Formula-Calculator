925e0c5699b36e5095533c67f0d73709bfb334e9
BRBergman <blake.bergman@gmail.com>
2022-09-14 12:47:43 -0400

    print center has its own file now

--- src/ncextra.cpp
+++ src/ncextra.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <iostream>
 #include <stdio.h>
+#include "printcenter.h"
 
 
 int error()
@@ -88,36 +89,5 @@ int add()
 	mvprintw(row/2,(col)/2,"%f",y);
 	getch();
 	return 0;
-}
-
-void printcenter(std::string in)
-{
-	mvprintw(row/2,(col-strlen(in.c_str()))/2,"%s",in.c_str()); //print string to center of screen
-	return;
-}
-
-void printcenter(char in[])
-{
-	mvprintw(row/2,(col-strlen(in))/2,"%s",in); //print char[] to center of screen
-	return;
-}
-
-void printcenter(float in)
-{
-	int conv = std::to_string(in).length();
-	mvprintw(row/2,(col-conv)/2,"%d",in); //print float to center of screen
-	return;
-}
-void printcenter(double in)
-{
-	int conv = std::to_string(in).length();
-	mvprintw(row/2,(col-conv)/2,"%d",in); //print double to center of screen
-	return;
-}
-void printcenter(int in)
-{
-	int conv = std::to_string(in).length();
-	mvprintw(row/2,(col-conv)/2,"%i",in); //print int to center of screen
-	return;
 }
 
