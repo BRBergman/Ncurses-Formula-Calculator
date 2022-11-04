a3d6ab584114e0cf6920d85b5fa2158528c74569
BRBergman <blake.bergman@gmail.com>
2022-11-04 13:22:17 -0400

    expanded printcenter comment

--- src/printcenter.h
+++ src/printcenter.h
@@ -6,7 +6,7 @@
 #include "ncextra.h"
 //print a message to the center of the screen, returns length 
 int printcenter (const char *format, ...);
-//print a message to the center of the screen, add offset, returns length
+//print a message to the center of the screen, add offset from middle, returns length
 int printcenter (int ver, int hoz, const char *format, ...);
 
 
