52db8ab8a8a6c05b616e120089aa2728c82c905c
BRBergman <blake.bergman@gmail.com>
2022-11-04 13:18:02 -0400

    removed perror() form printcenter and combinetext()

--- src/combinetext.cpp
+++ src/combinetext.cpp
@@ -7,7 +7,6 @@ std::string combinetext (const char *format, ...)
   	va_list args;
   	va_start (args, format);
   	vsprintf (buffer,format, args);
-  	perror (buffer);
   	va_end (args);
 	std::string in = buffer;
 	return in;
