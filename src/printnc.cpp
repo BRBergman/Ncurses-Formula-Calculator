8b51ea33263ac409bf846b66d11abca0915255cd
BRBergman <blake.bergman@gmail.com>
2022-11-07 12:54:02 -0500

    added comments and new printcn overloads

--- src/printnc.cpp
+++ src/printnc.cpp
@@ -38,3 +38,26 @@ int printnc (int ver, int hoz, const char *format, ...)
 	mvprintw(ver,hoz,"%s",in.c_str()); //print string to center of screen
 	return done;
 }
+int printnc (int ver, const char *format, ...)
+{
+   	char buffer[256];
+  	va_list args;
+  	va_start (args, format);
+  	int done = vsprintf (buffer,format, args);
+  	va_end (args);
+	std::string in = buffer;
+	mvprintw(ver,0,"%s",in.c_str()); //print string to center of screen
+	return done;
+}
+
+int printnc (const char *format, ...)
+{
+   	char buffer[256];
+  	va_list args;
+  	va_start (args, format);
+  	int done = vsprintf (buffer,format, args);
+  	va_end (args);
+	std::string in = buffer;
+	mvprintw(0,0,"%s",in.c_str()); //print string to center of screen
+	return done;
+}
--- src/printnc.h
+++ src/printnc.h
@@ -8,8 +8,14 @@
 int printnccenter (const char *format, ...);
 //print a message to the center of the screen, add offset from middle, returns length
 int printnccenter (int ver, int hoz, const char *format, ...);
-
+//prints message to the left of the screen, ver to go down, hoz to go to the side
 int printnc (int ver, int hoz, const char *format, ...);
+//prints message to the left of the screen, ver to go down
+int printnc (int ver, const char *format, ...);
+//prints message to top left of the screen
+int printnc (const char *format, ...);
+
+
 
 
 #endif
